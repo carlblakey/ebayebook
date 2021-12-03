a63a238b976870a6b0218bad3645fb572a06723b
Carl Blakey <csblakey@gmail.com>
2021-12-03 16:40:22 -0700

    update

--- ebook/js/main.js
+++ ebook/js/main.js
@@ -93,32 +93,32 @@ jQuery(document).ready(function ($) {
     }, 3000);
   });
   
-	var slideCount = $('.slider ul li').length;
-	var slideWidth = $('.slider ul li').width();
-	var slideHeight = $('.slider ul li').height();
+	var slideCount = $('#slider ul li').length;
+	var slideWidth = $('#slider ul li').width();
+	var slideHeight = $('#slider ul li').height();
 	var sliderUlWidth = slideCount * slideWidth;
 	
-	$('.slider').css({ width: slideWidth, height: slideHeight });
+	$('#slider').css({ width: slideWidth, height: slideHeight });
 	
-	$('.slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+	$('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
 	
-    $('.slider ul li:last-child').prependTo('.slider ul');
+    $('#slider ul li:last-child').prependTo('#slider ul');
 
     function moveLeft() {
-        $('.slider ul').animate({
+        $('#slider ul').animate({
             left: + slideWidth
         }, 700, function () {
-            $('.slider ul li:last-child').prependTo('.slider ul');
-            $('.slider ul').css('left', '');
+            $('#slider ul li:last-child').prependTo('#slider ul');
+            $('#slider ul').css('left', '');
         });
     }
 
     function moveRight() {
-        $('.slider ul').animate({
+        $('#slider ul').animate({
             left: - slideWidth
         },700, function () {
-            $('.slider ul li:first-child').appendTo('.slider ul');
-            $('.slider ul').css('left', '');
+            $('#slider ul li:first-child').appendTo('#slider ul');
+            $('#slider ul').css('left', '');
         });
     }
 
